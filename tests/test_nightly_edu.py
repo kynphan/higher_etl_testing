2bfc8ffe33a3b38c3d00dc268661837cb8b95604
Paola Andrea Garcia Cadavid <pgarcia@cafetosoftware.com>
2018-07-11 16:17:58 -0500

    Basic test structure

--- tests/test_nightly_edu.py
+++ tests/test_nightly_edu.py
@@ -4,11 +4,27 @@
 #
 
 import unittest
+import boto3
 
 import os
 import sys
 sys.path.append('..')  
 
 class test_nightly_edu(unittest.TestCase):
+
+    def setUp(self):
+
+        # access glue service
+        glue = boto3.client(
+            service_name='glue',
+            region_name='us-east-1',
+            endpoint_url='https://glue.us-east-1.amazonaws.com'
+            )
+
+        # Create CloudWatch client
+        cloudwatch = boto3.client('cloudwatch')
+
+    def test_EDUDirect_to_parquet_last_N_months(self):
+        job = glue.start_job_run( JobName=myJob['PlatformEvents_prices'] )
 
     unittest.main()
