b6b018a70dd71fc9b9fa5020ce8e089c7fd83481
Paola Andrea Garcia Cadavid <pgarcia@cafetosoftware.com>
2018-07-11 16:03:01 -0500

    Defined basic folder structure

---
+++ tests/test_nightly_edu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+#  Nightly scheduled jobs for EDU Direct
+#
+
+import unittest
+
+import os
+import sys
+sys.path.append('..')  
+
+class test_nightly_edu(unittest.TestCase):
+
+    unittest.main()
